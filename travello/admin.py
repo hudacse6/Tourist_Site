75504da6ca5dbb02f0c4fce32b18949a513a4807
Unknown <hudacse6@H.Rahman>
2020-06-18 13:59:02 +0600

    tourist_site

---
+++ travello/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Destination, News_blog
+
+# Register your models here.
+
+admin.site.register(Destination)
+
+admin.site.register(News_blog)
+
---
+++ travello/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='pics')
+    dis = models.CharField(max_length=500)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+
+
+class News_blog(models.Model):
+    img2 = models.ImageField(upload_to='blog_pics')
+    date = models.IntegerField()
+    month = models.CharField(max_length=20)
+    post_blog_title = models.CharField(max_length=100)
+    title_category = models.CharField(max_length=100)
+    post_blog_summary = models.CharField(max_length=300)
